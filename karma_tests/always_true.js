a6b79de2cc74bd69d0cc9abd7e08aef42cd7ff5a
Kyle Ladd <kladd6@gmail.com>
2016-05-30 11:48:25 -0400

    Karma Tests

---
+++ karma_tests/always_true.js
@@ -0,0 +1,10 @@
+describe('always true', function() {
+  it('Should be bar', function() {
+      expect(true).toBe(true);
+      expect(false).toBe(false);
+  });
+  it('Should be bar', function() {
+      expect(true).toBe(true);
+      expect(false).toBe(false);
+  });
+});
