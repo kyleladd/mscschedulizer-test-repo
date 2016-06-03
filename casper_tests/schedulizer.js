a345085696b654229c11325ab288e7fe9b024e80
Kyle Ladd <kladd6@gmail.com>
2016-06-03 10:25:17 -0400

    Settings screen size

--- casper_tests/schedulizer.js
+++ casper_tests/schedulizer.js
@@ -1,21 +1,10 @@
 casper.test.begin('MSCschedulizer', 2, function suite(test) {
+    casper.options.viewportSize = {width: 1600, height: 950};
     casper.start("http://schedulizer.morrisville.edu", function() {
         test.assertTitle("MSCschedulizer", "schedulizer homepage title is the one expected");
-        // test.assertExists('form[action="/search"]', "main form is found");
-        // this.fill('form[action="/search"]', {
-        //     q: "casperjs"
-        // }, true);
     });
-
-    // casper.then(function() {
-    //     test.assertTitle("casperjs - Recherche Google", "google title is ok");
-    //     test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
-    //     test.assertEval(function() {
-    //         return __utils__.findAll("h3.r").length >= 10;
-    //     }, "google search for \"casperjs\" retrieves 10 or more results");
-    // });
     casper.then(function() {
-        // Click on 1st result link
+        // Click on Get started button
         this.click('.slider.btn',10,10);
     });
     casper.then(function() {
