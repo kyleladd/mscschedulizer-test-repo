eb6ce5a06ccd97d93ce8e4788fc7b10f7cc24a16
Kyle Ladd <kladd6@gmail.com>
2016-06-04 12:58:25 -0400

    Generate page

--- casper_tests/schedulizer.js
+++ casper_tests/schedulizer.js
@@ -1,4 +1,4 @@
-casper.test.begin('MSCschedulizer', 2, function suite(test) {
+casper.test.begin('MSCschedulizer', 3, function suite(test) {
     casper.options.viewportSize = {width: 1600, height: 950};
     casper.start("http://schedulizer.morrisville.edu", function() {
         test.assertTitle("MSCschedulizer", "schedulizer homepage title is the one expected");
@@ -16,7 +16,20 @@ casper.test.begin('MSCschedulizer', 2, function suite(test) {
     casper.then(function() {
         this.capture('select-classes.png');
     });
-
+    casper.then(function() {
+        // Click on 1st result link
+        this.click('#dept_class_list a',10,10);
+    });
+    casper.then(function() {
+        this.clickLabel('Generate', 'a');
+    });
+    casper.then(function() {
+        test.assertUrlMatch(/generate.html/, "on the generate page");
+        this.waitForSelectorTextChange("#schedules");
+    });
+    casper.then(function() {
+        this.capture('generate.png');
+    });
     casper.run(function() {
         test.done();
     });
